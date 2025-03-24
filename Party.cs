8dbb02eda61e4249dd2d561c6481c09994945c8e
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2025-03-24 18:40:24 +0800

    Add project files.

---
+++ Party.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2 {
+    class Party {
+        private uint id;
+        private int dps = 0;
+        private int healer = 0;
+        private int tank = 0;
+
+        public Party(uint id) => this.id = id;
+
+        public bool addCharacter(string type) {
+            if (dps + healer + tank == 5) {
+                return false;
+            }
+            if (type == "Damage" && dps < 3) {
+                dps++;
+            }
+            else if (type == "Healer" && healer < 1) {
+                healer++;
+            }
+            else if (type == "Tank" && tank < 1) {
+                tank++;
+            }
+            return true;
+        }
+
+        public uint getId() { return id; }
+        public int getDps() { return dps; }
+        public int getHealer() { return healer; }
+        public int getTank() { return tank; }
+    }
+
+    class PartyManager {
+        //singleton that houses all parties
+        private static PartyManager? _instance;
+        private static readonly object _lock = new object();
+
+        private Dictionary<uint, Party> partyList = new Dictionary<uint, Party>();
+        private uint partyCount = 0;
+        private PartyManager() { }
+
+        public static PartyManager Instance {
+            get {
+                lock (_lock) {
+                    if (_instance == null) {
+                        _instance = new PartyManager();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public Party NewParty() {
+            uint id = partyCount++;
+            Party party = new Party(id);
+            partyList.Add(id, party);
+            return party;
+        }
+
+        public uint PartyCount() {  return partyCount; }
+
+        public Party? GetParty(uint id) {
+            foreach (KeyValuePair<uint, Party> party in partyList) {
+                if (party.Value.getId() == id) { return party.Value; }
+            }
+            return null;
+        }
+        
+        public void PrintParties() {
+            foreach (KeyValuePair<uint, Party> party in partyList) {
+                Console.WriteLine("Party " + party.Value.getId());
+                Console.WriteLine("\tDPS: " + party.Value.getDps());
+                Console.WriteLine("\tHealer: " + party.Value.getHealer());
+                Console.WriteLine("\tTank: " + party.Value.getTank());
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using P2;
+
+Console.WriteLine("Hello, World!");
+PartyManager pm = PartyManager.Instance;
+for (ulong i = 0; i < 5000; i++) {
+    pm.NewParty();
+}
+
+Console.WriteLine(pm.PartyCount() + " Parties");
