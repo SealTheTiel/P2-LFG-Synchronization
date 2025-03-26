d4f0a49b89492fc35ffe33a2401ce54c52ce5269
Sealtiel Dy <38400820+SealTheTiel@users.noreply.github.com>
2025-03-26 18:57:09 +0800

    add logger class

---
+++ Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2 {
+    class PreciseTime {
+        private static Stopwatch stopwatch = Stopwatch.StartNew();
+        private static DateTime dateTime = DateTime.UtcNow.AddHours(8);
+
+        public static string TimeStamp() {
+            long ticks = stopwatch.ElapsedTicks;
+            long ns = ticks * (1_000_000_000L / Stopwatch.Frequency);
+
+            DateTime time = dateTime.AddTicks(ns / 100);
+            return $"[{time:dd-MM-yyyy HH:mm:ss}.{ns % 1_000_000_000:D9}]:\t";
+        }
+    }
+    class Logger {
+        public Logger() { }
+        public void Log(string update, List<GameInstance> instances) {
+            Console.Write(PreciseTime.TimeStamp() + update);
+            foreach (GameInstance instance in instances) {
+                Console.Write("\n");
+                Console.Write($"\t\t\t\t{instance.GetId()}");
+            }
+            Console.Write("\n");
+        }
+    }
+}
